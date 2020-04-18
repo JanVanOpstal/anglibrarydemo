398ce6cd6da72e0628556618836f8d50a05b0762
nvision\janvanopstal <van_opstal_jan@telenet.be>
2020-04-18 20:35:54 +0200

    Second try to get it right

---
+++ projects/demo-library/src/lib/demo-library.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { DemoLibraryComponent } from './demo-library.component';
+
+
+
+@NgModule({
+  declarations: [DemoLibraryComponent],
+  imports: [
+  ],
+  exports: [DemoLibraryComponent]
+})
+export class DemoLibraryModule { }
